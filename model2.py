1d69fc68e7e32e33181a87a8405cf0d4709fd467
kovacikk <kovacikkyl@gmail.com>
2020-11-06 22:12:23 -0500

    Clustering of Dishes Works

--- model2.py
+++ model2.py
@@ -6,6 +6,8 @@ import random
 import math
 
 import json
+import copy
+random.seed(a= "slugcat2", version=2)
 
 dishesPath = './data/dishes.csv'
 trainPath = './data/user_ratings_train.json'
@@ -26,9 +28,13 @@ for dish in dishes:
     temp = np.zeros(len(dish[2:]))
     for i in range(len(dish[2:])):
         temp[i] = dish[i + 2]
+        #print(dish[i+2])
     dishNp[dish[0]] = np.array([temp])
+    #print(dish[0])
 
+#print(dishNp[200])
 
+#print(dishNp[0][0][:10])
 #print(len(dishes))
 
 
@@ -117,7 +123,7 @@ test = dishNp[0]
 
 #I don't know calculate L
 
-k = 5
+k = 20
 
 #Initialize Weights
 weights = {}
@@ -152,10 +158,18 @@ for i in range(k):
         for n in range(ingredients - j - 1):
             row.append(0)
         temp2.append(row)
-
-    clusterMean.append(dishNp[random.randint(0, len(dishes))])
+    value = random.randint(0, len(dishes))
+    test = dishNp[value]
+    clusterMean.append(test)
+    #print(value)
+    #print(dishNp[value])
+    #print(test)
+    #print("------------------------------------")
     clusterVariance.append(np.array(temp2))
 
+
+
+varianceNoise = clusterVariance[0]
 
 #print(clusterVariance[0])
 #print(clusterMean[0])
@@ -163,8 +177,16 @@ for i in range(k):
 #print(dishes[0][2:])
 
 
-def probability (x, mean, covariance):
-    determinant = np.linalg.det(covariance)
+def probability (x, mean, deter, inv):
+    determinant = deter
+    #determinant = np.linalg.det(covariance)
+
+    #if (determinant == 0):
+    #    covariance += varianceNoise
+    #    determinant = np.linalg.det(covariance)
+
+    #print(covariance, "\n----------\n", determinant)
+    
 
     norm_const = 1.0 / math.sqrt(2 * np.pi * determinant)
 
@@ -172,12 +194,10 @@ def probability (x, mean, covariance):
     error = x - mean
     #print(np.matmul(x.T, x))
     #print(np.matmul(error, np.linalg.inv(covariance)))
-
-    result = math.pow(math.e, -0.5 * (np.matmul(np.matmul(error, np.linalg.inv(covariance)), error.T)))
-
     
+    result = math.pow(math.e, -0.5 * np.linalg.multi_dot([error, inv, error.T]))
 
-    return norm_const * result 
+    return norm_const * result
 
 
 #print(probability(dishes[0][2:], clusterMean[2], clusterVariance[2]))
@@ -186,73 +206,186 @@ def probability (x, mean, covariance):
 
 # Run a Sum for the new mean and variance of a cluster
 
+prevClusters = clusterMean
+
+for iteration in range(1000):
+    prevprevClusters = copy.deepcopy(prevClusters)
+    prevClusters = copy.deepcopy(clusterMean)
+
+    # To Save Time Pre-calculate the Inverse Covariance and Determinant for Each Cluster
+    inv = {}
+    deter = {}
+    for i in range(k):
+        deter[i] = np.linalg.det(clusterVariance[i])
+        if (deter[i] == 0):
+            clusterVariance[i] = clusterVariance[i] + varianceNoise
+            deter[i] = np.linalg.det(clusterVariance[i])
+        inv[i] = np.linalg.inv(clusterVariance[i])
+
+    probs = {}
+    #To Save Time Calculate All Probabilities
+    for dish in dishes:
+        prob = {}
+        for i in range(k):
+            prob[i] = probability(dishNp[dish[0]], clusterMean[i], deter[i], inv[i])
+
+        probs[dish[0]] = prob
+
+    #Update Mean and Covariance for Each Cluster
+    for cluster in range(k):
+
+        mean = np.zeros(ingredients)
+        variance = np.zeros((ingredients, ingredients))
+
+        for dish in dishes:
+
+            #Get ProbXiC for Each Cluster
+            probSum = 0
+            for i in range(k):
+                probSum += probs[dish[0]][i] * (1.0/k)
+
+
+            # Get probCXi 
+            probCX = (probs[dish[0]][cluster] * (1.0/k)) / probSum
+
+            
+            probStuff = (probCX / ( (1.0/k) * len(dishes) ))
+
+            mean = np.add(mean, probStuff * dishNp[dish[0]])
+
+            error = dishNp[dish[0]] - mean
+            #print("Error:\n", error)
+        
+            #print(np.matmul(error.T, error))
+            #print("\n--------------\n", np.matmul(error.T, error))
+            #exit()
+            variance = np.add(variance, probStuff * np.matmul(error.T, error))
+            
+            #if (dish[0] == 1):
+                #exit()
+
+        clusterMean[cluster] = mean
+        clusterVariance[cluster] = variance
+
+        
+
+        #This is fucked for some reason
+
+    #print(clusterVariance[0])
+    #For Each Iteration
+    if (iteration % 5) == 0:
+        print("-------------------------------------")
+        print("Iteration: %d" %(iteration))
+        for kcluster in range(k):
+            print("Cluster %d: %f" % (kcluster, np.linalg.norm(prevprevClusters[kcluster] - clusterMean[kcluster])))
+            
+        #print("5: ", np.linalg.norm(lastCluster0 - clusterMean[5]))
+        #print(lastCluster0[0][:10])
+        #print(clusterMean[0][0][:10])
+        #print(clusterVariance[0][0][:10])
+        
+        #Check if all models have converged
+        count = 0
+        for kcluster in range(k):
+            if ((np.linalg.norm(prevprevClusters[kcluster] - clusterMean[kcluster])) < 0.005):
+                count = count + 1
+        #print(count)
+        
+        # All models are less than 0.005
+        if (count == k):
+            break
+
+        
+
+        #exit()
+    #if (np.array_equal(lastCluster0, clusterMean[0])):
+    #    print("true")
+    #    exit()
+
+
+    # Update mean and variance for guassians based on dish probability 
+
+
+    #Repeat Above until convergence is found
+
+#print
+#print(clusterVariance[0])
+
+
+
+
+# Test Probabilities for Clusters
+
 probs = {}
 #To Save Time Calculate All Probabilities
-for dish in dishes[:100]:
+
+#print("Dish:", dishNp[dishes[0][0]])
+
+inv = {}
+deter = {}
+for i in range(k):
+    deter[i] = np.linalg.det(clusterVariance[i])
+    if (deter[i] == 0):
+        clusterVariance[i] = clusterVariance[i] + varianceNoise
+        deter[i] = np.linalg.det(clusterVariance[i])
+    inv[i] = np.linalg.inv(clusterVariance[i])
+
+
+prob = {}
+
+for i in range(k):
+    prob[i] = probability(dishNp[dishes[0][0]], clusterMean[i], deter[i], inv[i])
+
+probs[dishes[0][0]] = prob
+
+for i in range(k):
+
+    probSum = 0
+    for j in range(k):
+        probSum += probs[dishes[0][0]][j] * (1.0/k)
+
+
+    # Get probCXi 
+    probCX = (probs[dishes[0][0]][i] * (1.0/k)) / probSum
+
+    print("Cluster % d: % f" %(i, probCX))
+    #print(clusterMean[i])
+
+
+
+
+
+
+
+inv = {}
+deter = {}
+for i in range(k):
+    deter[i] = np.linalg.det(clusterVariance[i])
+    if (deter[i] == 0):
+        clusterVariance[i] = clusterVariance[i] + varianceNoise
+        deter[i] = np.linalg.det(clusterVariance[i])
+    inv[i] = np.linalg.inv(clusterVariance[i])
+
+probs = {}
+#To Save Time Calculate All Probabilities
+for dish in dishes:
     prob = {}
     for i in range(k):
-        prob[i] = probability(dishNp[dish[0]], clusterMean[i], clusterVariance[i])
+        prob[i] = probability(dishNp[dish[0]], clusterMean[i], deter[i], inv[i])
 
     probs[dish[0]] = prob
 
-#print(probs[dish][cluster])
 
-#print(dishes[:100])
+for dish in dishes[:5]:
+    print("---------------------")
+    probSum = 0
+    for j in range(k):
+        probSum += probs[dish[0]][j] * (1.0/k)
 
-#Update Mean and Covariance for Each Cluster
-for cluster in range(k):
-
-    mean = np.zeros(ingredients)
-    variance = np.zeros((ingredients, ingredients))
-
-    for dish in dishes[:100]:
-
-        #Get ProbXiC for Each Cluster
-        probSum = 0
-        for i in range(k):
-            probSum += probs[dish[0]][i] * (1.0/k)
-
-
+    for i in range(k):
         # Get probCXi 
-        probCX = (probs[dish[0]][cluster] * (1.0/k)) / probSum
+        probCX = (probs[dish[0]][i] * (1.0/k)) / probSum
 
-        
-        probStuff = (probCX / ( (1.0/k) * len(dishes) ))
-
-        mean = np.add(mean, probStuff * dishNp[dish[0]])
-
-        error = dishNp[dish[0]] - mean
-        
-       
-        #print(np.matmul(error.T, error))
-        
-
-        variance = np.add(variance, probStuff * np.dot(error, error.T))
-        #print(variance)
-        #if (dish[0] == 1):
-            #exit()
-
-    clusterMean[cluster] = mean
-    clusterVariance[cluster] = variance
-    print(clusterVariance) #This is fucked for some reason
-
-
-#print(clusterMean)
-
-
-# Update mean and variance for guassians based on dish probability 
-
-
-#Repeat Above until convergence is found
-
-
-
-
-
-
-
-
-
-
-
+        print("Dish: %d | Cluster % d: % f" %(dish[0], i, probCX))
+        #print(clusterMean[i])
 
